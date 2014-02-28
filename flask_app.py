e68be2d5df792492bf7ef8ca5e05dfd2a09be6ed
Anderson Hamamoto <anderson.hamamoto@gmail.com>
2014-02-28 16:52:19 -0300

    paste de templates adicionado.

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('layout.html',
+        title = 'nuttyx',
+        some_phrase = 'Hello')
 
 if __name__ == "__main__":
     app.run()
